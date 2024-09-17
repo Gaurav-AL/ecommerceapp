ddf86e8fede59434b1a64f894e79ed260d9e292d
Gaurav Chaudhary <reverse454@gmail.com>
2024-09-17 22:49:40 +0530

    Latest commit add comments in JPA product repository

--- 02-backend/src/main/java/com/example/ecommerce/ecommerceapp/dao/ProductRepository.java
+++ 02-backend/src/main/java/com/example/ecommerce/ecommerceapp/dao/ProductRepository.java
@@ -1,13 +1,22 @@
 package com.example.ecommerce.ecommerceapp.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.ecommerce.ecommerceapp.entity.Product;
 
-@RepositoryRestResource(collectionResourceRel = "product", path = "product")
+@RepositoryRestResource(collectionResourceRel = "product", path = "products")
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
+
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    @Query("SELECT p FROM Product p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
 
 }
