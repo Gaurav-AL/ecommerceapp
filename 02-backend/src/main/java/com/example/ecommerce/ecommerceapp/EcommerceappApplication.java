9f07e938c8e97ae3b0eed91c8590f1ae919db2d0
Gaurav-AL <reverse454@gmail.com>
2025-01-22 23:51:10 +0530

    Resolved frontend behaviour subject not working

--- 02-backend/src/main/java/com/example/ecommerce/ecommerceapp/EcommerceappApplication.java
+++ 02-backend/src/main/java/com/example/ecommerce/ecommerceapp/EcommerceappApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication// Enable Scheduling a Job like deleting inactive sessions
+@EnableScheduling
 public class EcommerceappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceappApplication.class, args);
